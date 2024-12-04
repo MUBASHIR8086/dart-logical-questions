0ac62ef7abfd8c8ff56ab13394ee1fc7293d09de
MUBASHIR555-sof <mubashirtc54@gmail.com>
2024-12-04 10:25:57 +0530

    late

---
+++ dubliremovedowhile.dart
@@ -0,0 +1,14 @@
+void main(){
+  // dubli remove list use do while;
+   List<int>number=[1,2,3,4,5,6,7,8,9,10,1,1,1,2,2,2];
+  List<int>newnumber=[];
+  int index=0;
+  do {
+    if(!newnumber.contains(number[index])){
+      newnumber.add(number[index]);
+    }
+    index++;
+    
+  } while (index < number.length);
+  print(newnumber);
+}
