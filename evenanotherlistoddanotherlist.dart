7ed985d90a0175bfea9d9c5c74872802af7bb907
MUBASHIR8086 <mubashirtc54@gmail.com>
2025-02-01 15:39:11 +0530

    evenanotherlistoddanotherlist

---
+++ evenanotherlistoddanotherlist.dart
@@ -0,0 +1,16 @@
+void main() {
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  List<int> evenNumbers = [];
+  List<int> oddNumbers = [];
+
+  for (int number in numbers) {
+    if (number % 2 == 0) {
+      evenNumbers.add(number);
+    } else {
+      oddNumbers.add(number);
+    }
+  }
+
+  print('Even numbers: $evenNumbers');
+  print('Odd numbers: $oddNumbers');
+}
